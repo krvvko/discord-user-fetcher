a84defcafcab946ff01fd43a7e60f2240b5764b3
krvvko <krvvko@gmail.com>
2023-05-11 13:53:50 -0400

    home page time counter added

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,24 @@ const observer = new IntersectionObserver(
 );
 
 observer.observe(stickyElm)
+
+const uiElapsedElement = document.getElementById('home-discord-ui-elapsed');
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+function pad(n) {
+    return n < 10 ? '0' + n : n;
+}
+const uiElapsed = setInterval(() => {
+    seconds++;
+    if (seconds >= 60) {
+        minutes++;
+        seconds = 0;
+    }
+    if (minutes >= 60) {
+        hours++;
+        minutes = 0;
+    }
+    uiElapsedElement.innerText = `${pad(hours)}:${pad(minutes)}:${pad(seconds)} elapsed`;
+}, 1000)

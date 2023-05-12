6a28a21f482b4ee8ada9b7c7e0a30fabb7097e3b
krvvko <krvvko@gmail.com>
2023-05-12 16:45:15 -0400

    removed unused script

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,30 @@
 // get the sticky element
 const stickyElm = document.querySelector('header')
 
-const observer = new IntersectionObserver(
-    ([e]) => e.target.classList.toggle('isSticky', e.intersectionRatio < 1),
-    {threshold: [1]}
-);
-
-observer.observe(stickyElm)
-
-const uiElapsedElement = document.getElementById('home-discord-ui-elapsed');
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
-
-function pad(n) {
-    return n < 10 ? '0' + n : n;
-}
-const uiElapsed = setInterval(() => {
-    seconds++;
-    if (seconds >= 60) {
-        minutes++;
-        seconds = 0;
+window.onscroll = function() {
+    if (window.scrollY === 0) {
+        stickyElm.classList.remove("isSticky");
+    } else {
+        stickyElm.classList.add("isSticky");
     }
-    if (minutes >= 60) {
-        hours++;
-        minutes = 0;
-    }
-    uiElapsedElement.innerText = `${pad(hours)}:${pad(minutes)}:${pad(seconds)} elapsed`;
-}, 1000)
+};
+
+// const uiElapsedElement = document.getElementById('home-discord-ui-elapsed');
+// let seconds = 0;
+// let minutes = 0;
+// let hours = 0;
+// function pad(n) {
+//     return n < 10 ? '0' + n : n;
+// }
+// const uiElapsed = setInterval(() => {
+//     seconds++;
+//     if (seconds >= 60) {
+//         minutes++;
+//         seconds = 0;
+//     }
+//     if (minutes >= 60) {
+//         hours++;
+//         minutes = 0;
+//     }
+//     uiElapsedElement.innerText = `${pad(hours)}:${pad(minutes)}:${pad(seconds)} elapsed`;
+// }, 1000)
